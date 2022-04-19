820dd9238e91f94c696e1b9335b3bddb53fe547f
zahareliseev <zakh-e@yandex.ru>
2022-04-19 22:42:13 +0300

    Added command tests

--- src/hw4/rotation.h
+++ src/hw4/rotation.h
@@ -7,26 +7,9 @@
 
 #include "command.h"
 
-class IRotable {
-public:
-    virtual int get_direction() = 0;
 
-    virtual int get_angular_velocity() = 0;
-
-    virtual void set_direction(int newV) = 0;
-
-    virtual int get_directions_number() = 0;
-};
 namespace Command {
-    class RotateCommand {
-        IRotable *obj;
-    public:
-        explicit RotateCommand(IRotable *obj) : obj(obj) {}
 
-        void Execute() {
-            obj->set_direction((obj->get_direction() + obj->get_angular_velocity()) % obj->get_directions_number());
-        }
-    };
 }
 
 #endif //OTUS_ROTATION_H
